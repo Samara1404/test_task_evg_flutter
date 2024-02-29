5863d27083688228022d92b6f101676249645a14
Samara1404 <stambekova1404@gmail.com>
2024-02-29 23:35:42 +0300

    test_task_evg_flutter

---
+++ lib/home_page.dart
@@ -0,0 +1,110 @@
+import 'package:flutter/material.dart';
+import 'style.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          children: [
+            const Text('Логотип/картинка', style: AppTextStyle.style1),
+            const SizedBox(height: 50),
+            ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(5)),
+                    backgroundColor: Colors.purple,
+                    fixedSize: const Size(250, 40)),
+                onPressed: () {},
+                child: const Text('Регистрация', style: AppTextStyle.style2)),
+            const SizedBox(height: 8),
+            ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(5)),
+                    backgroundColor: Colors.grey,
+                    fixedSize: const Size(250, 40)),
+                onPressed: () {},
+                child: const Text('Пропустить', style: AppTextStyle.style2)),
+            const SizedBox(height: 10),
+            const Padding(
+              padding: EdgeInsets.all(60),
+              child: Row(
+                children: [
+                  Text('Вы тренер или другой специалист? ',
+                      style: AppTextStyle.style3),
+                  Text('Нажмите здесь', style: AppTextStyle.style4),
+                ],
+              ),
+            ),
+            const Text('Или зарегистрируйтесь с помощью'),
+            Padding(
+              padding: const EdgeInsets.all(20),
+              child: Center(
+                child: Row(
+                  children: [
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        fixedSize: const Size(80, 80),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(5)),
+                      ),
+                      onPressed: () {},
+                      child: Image.asset('/ic1.png'),
+                    ),
+                    const SizedBox(width: 30),
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        fixedSize: const Size(80, 80),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(5)),
+                      ),
+                      onPressed: () {},
+                      child: Image.asset('/ic2.png'),
+                    ),
+                    const SizedBox(width: 30),
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        fixedSize: const Size(80, 80),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(5)),
+                      ),
+                      onPressed: () {},
+                      child: Image.asset('/ic3.png'),
+                    ),
+                    const SizedBox(width: 30),
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        fixedSize: const Size(80, 80),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(5)),
+                      ),
+                      onPressed: () {},
+                      child: Image.asset('/ic4.png'),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.all(10),
+              child: Center(
+                  child: Text(
+                'Перед входом в приложение ознакомьтесь с Пользовательским согласшением и Политикой конфиденциальности',
+                style: AppTextStyle.style3,
+              )),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/style.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppTextStyle {
+  static const TextStyle style1 =
+      TextStyle(fontSize: 22, fontWeight: FontWeight.w600, color: Colors.black);
+       static const TextStyle style2 =
+      TextStyle(fontSize: 17, fontWeight: FontWeight.w600, color: Colors.white);
+       static const TextStyle style3 =
+      TextStyle(fontSize: 12, fontWeight: FontWeight.w400, color: Colors.black);
+        static const TextStyle style4=
+      TextStyle(fontSize: 12, fontWeight: FontWeight.w400, color: Colors.blue);
+}
